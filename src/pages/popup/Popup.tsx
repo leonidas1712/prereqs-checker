e0f5b66b7479060c462299b241de33b54a77aa16
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-30 00:21:48 +0800

    Refactor to ScrollContent

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
-import { Container, Text, Accordion, Center, Box, ScrollArea, Flex, Stack, Divider} from "@mantine/core";
+import { Container, Text, Accordion, Center, Box, ScrollArea, Flex, Stack, Divider, Space} from "@mantine/core";
 import { replicate } from "fp-ts/lib/Array";
 
 const Mods = () => {
@@ -22,40 +22,56 @@ const CONTENT_HEIGHT_PCT_INT=95;
 const CONTENT_HEIGHT_PCT=`${CONTENT_HEIGHT_PCT_INT}%`;
 const HEADER_HEIGHT_PCT=`${100-CONTENT_HEIGHT_PCT_INT}%`
 
-function Test2() {
-  const topStyle = {
-    flex: '1',
+type ScrollProps = {
+  style:React.CSSProperties
+};
+
+function ScrollContent(props:React.PropsWithChildren<ScrollProps>) {
+  const commonStyle = {
     overflow: 'auto',
+    padding:"0.5rem",
+  };
+  return (
+    <ScrollArea style={{...props.style, ...commonStyle}} type="auto" bg="blue">
+      { props.children }
+    </ScrollArea>
+  )
+}
+
+function Test3() {
+  const topStyle:React.CSSProperties = {
+    flex: '1',
+    backgroundColor:"cyan"
     // height: "20%",
   };
 
-  const bottomStyle = {
-    flex: '1.3',
-    overflow: 'auto',
+  const bottomStyle:React.CSSProperties = {
+    flex: '1.2',
+    backgroundColor:"green"
     // height: "80%",
   };
 
   return (
     <Flex direction={"column"} align={"stretch"} justify={"stretch"} style={{height:CONTENT_HEIGHT_PCT}}>
-      <ScrollArea style={topStyle} type="auto" bg="blue">
-       <Repeat n={20} text="Hi first"/>
-     </ScrollArea>
+      <ScrollContent style={topStyle}>
+       <Repeat n={30} text="Hi first"/>
+      </ScrollContent>
+      <Space/>
 
-     <ScrollArea style={bottomStyle} type="auto" bg="cyan">
-       <Repeat n={100} text="Hi second"/>
-     </ScrollArea>
+     <ScrollContent style={bottomStyle}>
+       <Repeat n={30} text="Hi second"/>
+     </ScrollContent>
     </Flex>
   );
 }
 
 function Repeat({ n, text }: { n:number, text:string })  {
   const mapper = (txt: string, idx: number) => {
-    return <Text fz="xl">{`${idx+1}. ${txt}`}</Text>
+    return <Text fz="lg">{`${idx+1}. ${txt}`}</Text>
   };
 
   return (
     <>
-      {/* { replicate(n, <Text fz="xl">{text}</Text>) } */}
       { replicate(n, text).map(mapper) }
     </>
   )
@@ -68,7 +84,7 @@ const Popup = () => {
         <Text fz="xl" align="center">Prerequisites Checker</Text>
       </Box>
 
-      <Test2/>
+      <Test3/>
     </Box>
   );
 };
