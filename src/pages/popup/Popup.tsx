37f01e655b586dd0c32a6b56d0422ebefbf79a6c
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-29 22:28:31 +0800

    Setup halves

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -2,20 +2,36 @@ import React from "react";
 import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 import { Container, Text, Accordion, Center } from "@mantine/core";
-import { Plus } from 'tabler-icons-react';
+import { replicate } from "fp-ts/lib/Array";
+
+const Mods = () => {
+  return <Accordion variant="separated" radius="sm" defaultValue="mods"
+  styles={{
+    item: {
+      width:"100%"
+    }}}>
+  <Accordion.Item value="mods">
+    <Accordion.Control>Add modules</Accordion.Control>
+    <Accordion.Panel>Panel content</Accordion.Panel>
+  </Accordion.Item>
+ </Accordion>
+};
+
+function Repeat({ n, text }: { n:number, text:string })  {
+  return (
+    <>
+      { replicate(n, <Text fz="xl">{text}</Text>) }
+    </>
+  )
+}
 
 const Popup = () => {
   return (
-     <Accordion variant="separated" radius="sm" defaultValue="mods"
-      styles={{
-        item: {
-          width:"100%"
-        }}}>
-      <Accordion.Item value="mods">
-        <Accordion.Control>Add modules</Accordion.Control>
-        <Accordion.Panel>Panel content</Accordion.Panel>
-      </Accordion.Item>
-     </Accordion>
+     <>
+      <Repeat n={30} text="Hi first"/>
+      <Repeat n={30} text="Hi second"/>
+
+     </>
   );
 };
 
