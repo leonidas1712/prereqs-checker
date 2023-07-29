1421d9fb0453eb01d4e1f0cb43032d00f3411b85
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-29 18:19:54 +0800

    Clear default styles

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -4,21 +4,8 @@ import "@pages/popup/Popup.css";
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-lime-400">
-          hi <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div>
+      <p>hi</p>
     </div>
   );
 };
