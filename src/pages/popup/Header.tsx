e424d476176a1b0790a0a8c13fc204d4fade600d
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-31 02:32:24 +0800

    Add color to eye icon

--- src/pages/popup/Header.tsx
+++ src/pages/popup/Header.tsx
@@ -2,19 +2,23 @@ import { Text, Flex, Tooltip, ActionIcon, useMantineTheme } from "@mantine/core"
 import { ShowModsProps, HEADER_HEIGHT_PCT } from "./common";
 import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
+const ICON_SIZE="1.2rem";
+
 function ToggleModsBtn(props:ShowModsProps) {
     const { showMods, setShowMods } = props;
+    const theme = useMantineTheme();
+
     const click = () => {
       console.log("Show mods", showMods);
       setShowMods(!showMods);
     };
   
     const label=showMods ? "Hide mods" : "Show mods";
-    const icon = showMods ? <FaEyeSlash size="1.1rem"/> : <FaEye size="1.1rem" />
+    const icon = showMods ? <FaEyeSlash size={ICON_SIZE} color={theme.other.secondaryFontColor}/> : <FaEye size={ICON_SIZE} color={theme.other.secondaryFontColor}/>
   
     return (
-      <Tooltip label={label} color="teal">
-        <ActionIcon component="button" onClick={click} color="yellow">{icon}</ActionIcon>
+      <Tooltip label={label} color={theme.other.priOrange}>
+        <ActionIcon component="button" onClick={click} color="yellow" ml="0.2rem">{icon}</ActionIcon>
       </Tooltip>
     )
   }
