30bd43b7611101d952f8ee2055712f6972ff8672
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-31 01:22:38 +0800

    Add get_mods

--- src/pages/background/helpers.ts
+++ src/pages/background/helpers.ts
@@ -1,3 +1,9 @@
+import axiosRetry from 'axios-retry';
+import axios from 'axios';
+
+axiosRetry(axios, { retries: 3 });
+
+
 // For requesting mods
 type ModuleCode = string; // E.g. "CS3216"
 type ModuleTitle = string;
@@ -9,6 +15,12 @@ type ModuleCondensed = Readonly<{
 
 const mod_list = "https://api.nusmods.com/v2/2023-2024/moduleList.json";
 
-async function get_mods() {
-    return 2;
+async function get_mods():Promise<ModuleCondensed[]> {
+    try {
+        const res:ModuleCondensed[] = await axios.get(mod_list);
+        console.log("Mods:", res.slice(0,5));
+        return Promise.resolve(res);
+    } catch (error) {
+        return Promise.reject(`Error getting module list: ${error}`);
+    }
 }
