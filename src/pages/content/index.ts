3f8842b797212d12286ff5f92718a88fccb4ea0a
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-30 10:54:29 +0800

    Add listener to content script

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -6,15 +6,29 @@ console.log("Detected NUSMods course page changed");
  */
 // import("./components/Demo");
 
-(async () => {
-    console.log("Sending from content script");
-    const response = await chrome.runtime.sendMessage("hi from content script");
-    console.log("Sent from content script");
-
-    // do something with response here, not outside the function
-    console.log(response);
-  })();
-
 // (async () => {
-//     console.log("Sending from content script async");
-// })();
+//     console.log("Sending from content script");
+//     const response = await chrome.runtime.sendMessage("hi from content script");
+//     console.log("Sent from content script");
+
+//     // do something with response here, not outside the function
+//     console.log(response);
+//   })();
+
+
+const messagesFromReactAppListener = (
+  msg,
+  sender: chrome.runtime.MessageSender,
+  sendResponse: (response) => void) => {
+ 
+  console.log('[content.js]. Message received', msg);
+
+   // Prepare the response object with information about the site
+  const response = {
+      title: document.title,
+  };
+
+  sendResponse(response);
+}
+
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
