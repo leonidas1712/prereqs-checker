a66e35869097150ccb489795bfe82ecab3c6d603
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 16:55:32 +0800

    Update package.json

--- src/__tests__/todo.test.js
+++ src/__tests__/todo.test.js
@@ -1,3 +1,10 @@
-// test('test', () => {
-//     expect(true).toBe(true);
-// });
+import { test, expect } from "vitest";
+import { render, screen, cleanup } from '@testing-library/react'
+
+test('test', () => {
+    expect(true).toBe(true);
+});
+
+test('failtest', () => {
+    expect(true).toBe(true);
+});
