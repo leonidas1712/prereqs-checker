b8a9eeded06c25bcae352b0bdba57c95de594ea1
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 22:08:21 +0800

    Add sleep

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@pages/popup/index.css";
 import Popup from "@pages/popup/Popup";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Loader } from "@mantine/core";
 import { requestModuleFromContentScript } from "./helpers";
 import { DarkThemeOverride } from "./themes";
 import * as Opt from "fp-ts/lib/Option";
@@ -13,25 +13,33 @@ refreshOnUpdate("pages/popup");
 
 
 // Refresh the useEffect here when content title changes
+// React.SetStateAction<Opt.Option<Module>>
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
 
 // set module here: pass down Option<Module> to popup
 function Root() {
   const [module, setModule] = useState<Opt.Option<Module>>(Opt.none);
-
+  // const [loading, setLoading] = useState(true);
 
   // To request for module upon initial page load (errs when not on nusmods.com)
   useEffect(() => {
     requestModuleFromContentScript()
       .then((res) => { 
         console.log("Res from content script:", res);
-        setModule(Opt.some(res));
+
+        // Slow down to test loading spinner
+        sleep(2000).then((_) => {
+          // setLoading(false);
+          setModule(Opt.some(res));
+        });
       })
       .catch((err) => console.log("Error while requesting:", err));
 
   },[]);
   return (
     <MantineProvider theme={DarkThemeOverride}>
-      <Popup module={module} />
+        <Popup module={module} />
     </MantineProvider>
   )
 }
