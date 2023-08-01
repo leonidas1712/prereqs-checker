510aa0d7f50b917655592124b93a348f1d3faa2c
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 20:58:26 +0800

    Add tests for parse response

--- src/pages/popup/index.test.tsx
+++ src/pages/popup/index.test.tsx
@@ -8,8 +8,7 @@ import { chrome } from 'jest-chrome'
 import { Module } from '@src/common';
 
 describe("parsing of module from content script response", () => {
-    it("parses valid response correctly", () => {
-        console.log("Hello");
+    it("parses valid input correctly", () => {
         const valid = "CS2113 Software Engineering & Object-Oriented Programming - NUSMods";
         const res = getModuleFromContentResponse(valid);
         console.log(res);
@@ -20,7 +19,30 @@ describe("parsing of module from content script response", () => {
             expect(module.moduleCode).toBe("CS2113");
             expect(module.title).toBe('Software Engineering & Object-Oriented Programming');
         });
-        
+    });
+
+    it("parses invalid input on NUSMods domain correctly (returns none)", () => {
+        const invalid = "Courses - NUSMods";
+        const res = getModuleFromContentResponse(invalid);
+        console.log(res);
+
+        match<Module, void>(() => {
+            expect(true).toBe(true);
+        }, (_) => {
+            throw new Error("getModuleFromContentResponse returned module on invalid input");
+        });
+    });
+
+    it("parses invalid input NOT on NUSMods domain correctly (returns none)", () => {
+        const invalid = "Example | Testing Library";
+        const res = getModuleFromContentResponse(invalid);
+        console.log(res);
+
+        match<Module, void>(() => {
+            expect(true).toBe(true);
+        }, (_) => {
+            throw new Error("getModuleFromContentResponse returned module on invalid input");
+        });
     });
 });
 
