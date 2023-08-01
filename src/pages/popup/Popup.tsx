22134ae20825a2e4961d886ed77ff291cb3442d9
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 22:11:35 +0800

    Change Popup box to use theme bg

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "@pages/popup/Popup.css";
-import { Box } from "@mantine/core";
+import { Box, useMantineTheme } from "@mantine/core";
 
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { ModuleProps } from "./common";
+// change Box bg to use theme
 
 const Popup = (props:ModuleProps & { loading:boolean }) => {
   // useEffect(() => {
@@ -14,10 +15,12 @@ const Popup = (props:ModuleProps & { loading:boolean }) => {
   //   });
   // });
 
+  const theme = useMantineTheme();
+
   // initially don't show mods
   const [showMods, setShowMods] = useState(false);
   return (
-      <Box h="100%" bg="#333">  
+      <Box h="100%" bg={theme.other.bgColor}>  
         <Header module={props.module} showMods={showMods} setShowMods={setShowMods}/>
         <Content module={props.module} showMods={showMods} setShowMods={setShowMods}/>
       </Box>
