63197744bd0301a3d6688c534097b503edb0dddb
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 20:38:50 +0800

    Add jest-chrome

--- src/pages/popup/index.test.tsx
+++ src/pages/popup/index.test.tsx
@@ -1,3 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test, vi } from 'vitest';
 import '@testing-library/jest-dom';
+
+test("mock chrome", () => {
+    expect(true).toBe(true);
+});
