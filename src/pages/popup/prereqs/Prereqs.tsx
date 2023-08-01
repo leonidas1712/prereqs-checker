89beab4e59c863930f333217439b722bd789c329
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-01 12:59:43 +0800

    Remove unused code

--- src/pages/popup/prereqs/Prereqs.tsx
+++ src/pages/popup/prereqs/Prereqs.tsx
@@ -4,28 +4,6 @@ import { ModuleProps } from "../common";
 import { match } from "fp-ts/lib/Option";
 import { Module } from "@src/common";
 
-// function ModDescription(props:{ module: Module }) {
-//     const theme = useMantineTheme();
-//     const mod = props.module;
-//     console.log("Module in module desc:", mod);
-//     return (
-//         <>
-//             <Center>
-//                 <Text fz={theme.other.titleFontSize} fw={theme.other.titleFontWeight}>{mod.moduleCode}</Text>
-//             </Center>
-
-//             <Center>
-//                 <Text 
-//                 c={theme.other.secondaryFontColor} 
-//                 fz={theme.other.subtitleFontSize} 
-//                 fw={theme.other.titleFontWeight}
-//                 align="center"
-//                 >{mod.title}</Text>
-//             </Center>
-//         </>
-//     );
-// }
-
 // Later: add prereqTree
 export type PrereqsProps = ModuleProps;
 
